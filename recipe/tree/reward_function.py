4452541a90497179c45ba72d83040ce629f0c758
symoon11 <symoon11@snu.ac.kr>
2025-08-28 23:37:14 +0900

    Update liger loss

--- recipe/tree/reward_function.py
+++ recipe/tree/reward_function.py
@@ -20,7 +20,6 @@ from verl.utils.reward_score import math_verify
 def compute_score(data_source: str, solution_str: str, ground_truth: str, extra_info: dict[str, Any]) -> float:
     if "</think>" in solution_str:
         model_output = solution_str.split("</think>")[-1]
-        score = math_verify.compute_score(model_output, ground_truth)
+        return math_verify.compute_score(model_output, ground_truth)
     else:
-        score = 0.0
-    return score
+        return 0.0
